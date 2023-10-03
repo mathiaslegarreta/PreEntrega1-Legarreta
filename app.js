0f9f21ec13407f75036b700f9a22269e7afdb734
Mathias Legarreta <mathiaslegarreta18@hotmail.com>
2023-10-03 20:16:51 -0300

    reflejar elecciones

--- app.js
+++ app.js
@@ -1,63 +1,54 @@
-const resultsJSON = [];
+let user;
+let pc;
+let roundResult;
 const min = 1;
 const max = 3;
-
 const choices = {
     1: "Piedra",
     2: "Papel",
     3: "Tijera"
 };
 
-function updateDOM(userChoice, pcChoice, roundResult) {
-    const userChoiceImage = document.getElementById("user-choice");
-    const pcChoiceImage = document.getElementById("cpu-choice");
-    const resultText = document.getElementById("result");
-
-    userChoiceImage.src = `images/${choice(userChoice).toLowerCase()}.png`;
-    pcChoiceImage.src = `images/${choice(pcChoice).toLowerCase()}.png`;
-
-    resultText.textContent = `Resultado de la ronda ${resultsJSON.length}: ${roundResult}`;
-}
-
-
-function choice(election) {
-    return choices[election] || "TU ELECCIÓN NO ES CORRECTA!!";
-}
-
-do {
-    user = parseInt(prompt("Elige: 1-Piedra, 2-Papel o 3-Tijera"));
-
-    if (user !== 1 && user !== 2 && user !== 3) {
-        alert("ELECCIÓN INCORRECTA. Por favor, elige 1, 2 o 3.");
-        continue;
-    }
-
+// Función para realizar una ronda del juego y actualizar la página
+function playRound(userChoice) {
+    user = parseInt(userChoice);
     pc = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert("El Usuario elige " + choice(user));
-    alert("PC elige " + choice(pc));
+    const userChoiceText = choice(user);
+    const pcChoiceText = choice(pc);
 
+    const resultElement = document.getElementById('result');
+    const userChoiceImage = document.getElementById('user-choice');
+    const cpuChoiceImage = document.getElementById('cpu-choice');
+
+    userChoiceImage.src = `images/${userChoiceText.toLowerCase()}.png`;
+    cpuChoiceImage.src = `images/${pcChoiceText.toLowerCase()}.png`;
 
     if (user == pc) {
-        alert("EMPATE");
         roundResult = "EMPATE";
     } else if (
         (user == 1 && pc == 3) ||
         (user == 2 && pc == 1) ||
         (user == 3 && pc == 2)
     ) {
-        alert("GANA EL USUARIO");
         roundResult = "GANA EL USUARIO";
     } else {
-        alert("GANA LA MÁQUINA");
         roundResult = "GANA LA MÁQUINA";
     }
 
-    results.push(roundResult);
+    resultElement.textContent = roundResult;
+}
 
-} while (results.length < 3);
-
-results.forEach((result, index) => {
-    alert(`Resultado de la ronda ${index + 1}: ${result}`);
+// Agregar escuchadores de eventos para las opciones de usuario
+const optionImages = document.querySelectorAll('.option-image');
+optionImages.forEach(optionImage => {
+    optionImage.addEventListener('click', function() {
+        const userChoice = optionImage.getAttribute('data-choice');
+        playRound(userChoice);
+    });
 });
 
+// Función para obtener el nombre de la elección
+function choice(election) {
+    return choices[election] || "TU ELECCIÓN NO ES CORRECTA!!";
+}
