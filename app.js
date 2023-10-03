99451a487ab75098311a0bbf229f690edf1c1dd6
Mathias Legarreta <mathiaslegarreta18@hotmail.com>
2023-10-03 20:34:30 -0300

    modificaion textos y 5 rondas

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ const choices = {
 };
 
 const results = []; 
+let roundsPlayed = 0; 
+const roundsToPlay = 5; 
 
 function playRound(userChoice) {
+    if (roundsPlayed >= roundsToPlay) {
+        return;
+    }
+
     user = parseInt(userChoice);
     pc = Math.floor(Math.random() * (max - min + 1)) + min;
 
@@ -40,6 +46,12 @@ function playRound(userChoice) {
     resultElement.textContent = roundResult;
 
     results.push(roundResult);
+
+    roundsPlayed++;
+
+    if (roundsPlayed >= roundsToPlay) {
+        determineWinner();
+    }
 }
 
 const optionImages = document.querySelectorAll('.option-image');
@@ -54,6 +66,28 @@ function choice(election) {
     return choices[election] || "TU ELECCIÓN NO ES CORRECTA!!";
 }
 
+function determineWinner() {
+    const resultElement = document.getElementById('result');
+
+    const userWins = results.filter(result => result === "GANA EL USUARIO").length;
+    const cpuWins = results.filter(result => result === "GANA LA MÁQUINA").length;
+
+    if (userWins > cpuWins) {
+        resultElement.textContent = "EL USUARIO HA GANADO EL JUEGO";
+        storeWinner("Usuario");
+    } else if (cpuWins > userWins) {
+        resultElement.textContent = "LA PC HA GANADO EL JUEGO";
+        storeWinner("Máquina");
+    } else {
+        resultElement.textContent = "¡EMPATE GENERAL!";
+        storeWinner("Empate");
+    }
+}
+
+function storeWinner(winner) {
+    localStorage.setItem("gameWinner", winner);
+}
+
 function restartGame() {
     const userChoiceImage = document.getElementById('user-choice');
     const cpuChoiceImage = document.getElementById('cpu-choice');
@@ -61,11 +95,23 @@ function restartGame() {
     userChoiceImage.src = 'images/piedra.png';
     cpuChoiceImage.src = 'images/piedra.png';
 
+    
     const resultElement = document.getElementById('result');
     resultElement.textContent = 'A Jugar!';
 
+   
     results.length = 0;
+
+    localStorage.removeItem("gameWinner");
+
+    roundsPlayed = 0;
 }
 
 const restartButton = document.getElementById('restart');
 restartButton.addEventListener('click', restartGame);
+
+const storedWinner = localStorage.getItem("gameWinner");
+if (storedWinner) {
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `${storedWinner} HA GANADO EL JUEGO`;
+}
