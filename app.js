14f5e9e409a7941b17032a7a7ea9432a5991599c
Mathias Legarreta <mathiaslegarreta18@hotmail.com>
2023-10-05 19:29:27 -0300

    emprolijo

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-let user;
-let pc;
-let roundResult;
 const min = 1;
 const max = 3;
 const choices = {
@@ -8,6 +5,7 @@ const choices = {
     2: "Papel",
     3: "Tijera"
 };
+
 const results = {
     historial: [],
     roundsPlayed: 0,
@@ -15,7 +13,12 @@ const results = {
     gameWinner: null
 };
 
-function playRound(userChoice) {
+let user;
+let pc;
+let roundResult;
+
+
+function playGameRound(userChoice) {
     if (results.roundsPlayed >= results.roundsToPlay) {
         determineWinner();
         return;
@@ -27,26 +30,21 @@ function playRound(userChoice) {
     const userChoiceText = choice(user);
     const pcChoiceText = choice(pc);
 
-    const resultElement = document.getElementById('result');
-    const userChoiceImage = document.getElementById('user-choice');
-    const cpuChoiceImage = document.getElementById('cpu-choice');
+    displayChoices(userChoiceText, pcChoiceText);
 
-    userChoiceImage.src = `images/${userChoiceText.toLowerCase()}.png`;
-    cpuChoiceImage.src = `images/${pcChoiceText.toLowerCase()}.png`;
-
-    if (user == pc) {
+    if (user === pc) {
         roundResult = "EMPATE";
     } else if (
-        (user == 1 && pc == 3) ||
-        (user == 2 && pc == 1) ||
-        (user == 3 && pc == 2)
+        (user === 1 && pc === 3) ||
+        (user === 2 && pc === 1) ||
+        (user === 3 && pc === 2)
     ) {
         roundResult = "GANA EL USUARIO";
     } else {
         roundResult = "GANA LA MÁQUINA";
     }
 
-    resultElement.textContent = roundResult;
+    updateResult(roundResult);
     results.historial.push(roundResult);
     results.roundsPlayed++;
 
@@ -55,8 +53,8 @@ function playRound(userChoice) {
     }
 }
 
-function choice(election) {
-    return choices[election] || "TU ELECCIÓN NO ES CORRECTA!!";
+function choice(selection) {
+    return choices[selection] || "TU ELECCIÓN NO ES CORRECTA!!";
 }
 
 function determineWinner() {
@@ -70,34 +68,42 @@ function determineWinner() {
     const cpuWins = results.historial.filter(result => result === "GANA LA MÁQUINA").length;
 
     if (userWins > cpuWins) {
-        results.gameWinner = "Usuario";
-        resultElement.textContent = "EL USUARIO HA GANADO EL JUEGO";
-        storeWinner("Usuario");
+        announceWinner("Usuario", "EL USUARIO HA GANADO EL JUEGO");
     } else if (cpuWins > userWins) {
-        results.gameWinner = "Máquina";
-        resultElement.textContent = "LA PC HA GANADO EL JUEGO";
-        storeWinner("Máquina");
-    } else if (userWins === results.roundsToPlay) {
-        results.gameWinner = "Empate";
-        resultElement.textContent = "¡EMPATE GENERAL!";
-        storeWinner("Empate");
+        announceWinner("Máquina", "LA PC HA GANADO EL JUEGO");
+    } else {
+        announceWinner("Empate", "¡EMPATE GENERAL!");
     }
+}
+
+function announceWinner(winner, message) {
+    results.gameWinner = winner;
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = message;
+    storeWinner(winner);
 }
 
 function storeWinner(winner) {
     localStorage.setItem("gameWinner", winner);
-
-    if (winner === "Empate" && results.roundsPlayed === results.roundsToPlay) {
-        const historialDiv = document.getElementById("historial");
-        const resultado = document.createElement("p");
-        resultado.textContent = winner;
-        historialDiv.appendChild(resultado);
-    } else if (results.roundsPlayed === results.roundsToPlay) {
+    if (results.roundsPlayed === results.roundsToPlay) {
         const historialDiv = document.getElementById("historial");
         const resultado = document.createElement("p");
         resultado.textContent = winner;
         historialDiv.appendChild(resultado);
     }
+}
+
+function updateResult(result) {
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = result;
+}
+
+function displayChoices(userChoice, pcChoice) {
+    const userChoiceImage = document.getElementById('user-choice');
+    const cpuChoiceImage = document.getElementById('cpu-choice');
+
+    userChoiceImage.src = `images/${userChoice.toLowerCase()}.png`;
+    cpuChoiceImage.src = `images/${pcChoice.toLowerCase()}.png`;
 }
 
 function restartGame() {
@@ -107,8 +113,7 @@ function restartGame() {
     userChoiceImage.src = 'images/piedra.png';
     cpuChoiceImage.src = 'images/piedra.png';
 
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = 'A Jugar!';
+    updateResult('A Jugar!');
 
     results.historial.length = 0;
     localStorage.removeItem("gameWinner");
@@ -130,6 +135,6 @@ const optionImages = document.querySelectorAll('.option-image');
 optionImages.forEach(optionImage => {
     optionImage.addEventListener('click', function () {
         const userChoice = optionImage.getAttribute('data-choice');
-        playRound(userChoice);
+        playGameRound(userChoice);
     });
 });
